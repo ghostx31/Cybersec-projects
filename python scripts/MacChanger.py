8f2d661c1b5e37e76ba69a67cfd234996487c948
ghostx31 <himanshu@kali.kali>
2021-06-18 20:07:23 +0530

    Added networkScanner.py and edited MacChanger.py

--- python scripts/MacChanger.py
+++ python scripts/MacChanger.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python 
 
 import subprocess
-import optparse
+import argparse as ap 
 import re
 
-def input_data():
-    objParse = optparse.OptionParser()
-    objParse.add_option("-i", "--interface", dest="interface", help="Interface to change the MAC address. ")
-    objParse.add_option("-m", "--mac", dest="new_mac", help="The new MAC address. ")
-    (opts, args) = objParse.parse_args()
+def input_data():                                                       # Def to input interface and MAC address from the user. 
+    objParse = ap.ArgumentParser()
+    objParse.add_argument("-i", "--interface", dest="interface", help="Interface to change the MAC address. ")
+    objParse.add_argument("-m", "--mac", dest="new_mac", help="The new MAC address. ")
+    opts = objParse.parse_args()
     if not opts.interface:
-        objParse.error("[-] Please mention an interface. ")
-    elif not opts.new_mac:
-        objParse.error("[-] Please mention a MAC address. ")
+        objParse.error("[-] Please mention an interface. ")             # Interface not passed  
+    elif not opts.new_mac:      
+        objParse.error("[-] Please mention a MAC address. ")            # MAC address not passed. 
     return opts
 
-def mac_change(interface, new_mac):
+def mac_change(interface, new_mac):                                     # Def to perform the operation of changing the MAC address
     print('[+] Changing MAC address for ' + interface + " to " + new_mac) 
     subprocess.call(["ifconfig", interface, "down"]) 
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-def foundMac(interface):
+def foundMac(interface):                                                   # Def to check whether the MAC was changed as requested by the user 
     res = subprocess.check_output(["ifconfig", opts.interface])
     regex = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", res)
     if regex:
@@ -30,12 +30,14 @@ def foundMac(interface):
         print("[-] Could not read the MAC address. ") 
 opts = input_data()
 currMAC = foundMac(opts.interface)
-print("Current MAC address = " + str(currMAC)) 
+
+print("Current MAC address = " + str(currMAC))                             # Printing current MAC address 
 mac_change(opts.interface, opts.new_mac)
+
 currMAC = foundMac(opts.interface)
 if currMAC == opts.new_mac:
-    print("[+] MAC address was changed to: " + opts.new_mac)
-    print("Calling ifconfig for " + opts.interface + " :")
+    print("[+] MAC address was changed to: " + opts.new_mac)                # New MAC address
+    print("Calling ifconfig for " + opts.interface + " :")                  # Call ifconfig for the interface for which MAC was changed. 
     subprocess.call(["ifconfig", opts.interface])
 else:
     print("[-] Could not change the MAC address. ")
--- python scripts/networkScanner.py
+++ python scripts/networkScanner.py
@@ -1,8 +1,37 @@
 #! /usr/bin/env python
 
-import scapy.all as scapy 
+from re import VERBOSE
+import scapy.all as sc
+import argparse as ap
 
-def ipscan(ip):
-    scapy.arping(ip)
+def get_args():                                     # Def to get the IP address from the user 
+    obj = ap.ArgumentParser()
+    obj.add_argument("-i", "--ip", dest="address", help="Target IP / Range IP")
+    opts = obj.parse_args()
+    if not opts.address:
+        obj.error("[-] Please mention an IP address. [-]")
+    return opts
 
-ipscan("192.168.122.1/24")
+def arp_scan(ip):                                   # Def to generate ARP request
+    req = sc.ARP(pdst=ip)                           #ARP request
+    broadcast = sc.Ether(dst="ff:ff:ff:ff:ff:ff")   #Broadcast signal
+    arp_broadcast = broadcast/req                   #Add ARP request and broadcast signal together
+    ans = sc.srp(arp_broadcast, timeout=1, verbose=False)[0]  #making list answered to filter out the required part.
+
+   
+    dev_list = []
+    for e in  ans:
+        dev_dict = {"ip" : e[1].psrc, "mac" : e[1].hwsrc}
+        dev_list.append(dev_dict) 
+    return  (dev_list)
+
+def print_res(res):                             # Def to print the output of the devices found 
+    print("IP Address\t\t\tMAC Address")
+    print("\n[-------------------------------------------------------------------------------]")
+    for dev in res:
+        print(dev["ip"] + "\t\t" + dev["mac"])          # Print from dictionary for ip and mac
+        print("[-------------------------------------------------------------------------------]") 
+
+opts = get_args()
+scan_res = arp_scan(opts.address)
+print_res(scan_res)
